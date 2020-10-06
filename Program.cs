abfa83943a13b680e4167642a3f8cab5e6c6109a
dheer meena <dheer1998meena@gmail.com>
2020-10-07 00:39:34 +0530

    [Dheer] Add . PinCodeProblem

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PinCodeProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the indian pin code number");
+            string pinCode = Console.ReadLine();
+            RegexValidation reg = new RegexValidation();
+            reg.Validation(pinCode);
+        }
+    }
+}
---
+++ RegexValidation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PinCodeProblem
+{
+    class RegexValidation
+    {
+        public void Validation(string pinCode)
+        {
+            Regex regex = new Regex("[1-9]{1}[0-9]{5}");
+            
+            if (regex.IsMatch(pinCode))
+            {
+                Console.WriteLine("The PIN code is Valid");
+            }
+            else
+            {
+                Console.WriteLine("The PIN code is not Valid");
+            }
+           
+        }
+    }
+}
